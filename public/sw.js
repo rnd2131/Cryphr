3059549a899168f86957754fcfa8d6d3f1092046
rnd2131 <radmandarkhosh18@gmail.com>
2024-07-05 03:16:33 +0330

    cash+/\

--- public/sw.js
+++ public/sw.js
@@ -8,6 +8,53 @@ importScripts(__uv$config.sw || '/uv/uv.sw.js');
 const sw = new UVServiceWorker();
 const dynamic = new Dynamic();
 
+const cacheName = 'v1';
+const cacheAssets = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/images/image.jpg',
+  '/news.html',
+  '/newsSR.html'
+];
+
+// Install Event
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        console.log('Caching files');
+        return cache.addAll(cacheAssets);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', e => {
+  // Remove old caches
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
+
 self.dynamic = dynamic;
 
 self.addEventListener('fetch', event => {
